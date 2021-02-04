3112796e0e3080feca2048cd30c5ece0db3527da
清香白莲 <1243327527@qq.com>
2021-02-04 17:25:38 +0800

    继续测试

--- SwitStudyFight/Modules/Controller/TitleListVC.swift
+++ SwitStudyFight/Modules/Controller/TitleListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TitleListVC.swift
 //  SwitStudyFight
 //
 //  Created by mingxing on 2021/2/4.
@@ -7,16 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TitleListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        self.title = "Swift学习"
+
+        //        self.title = "Swift学习"
         self.title = "加油！！！"
-        
     }
 
-
 }
-
